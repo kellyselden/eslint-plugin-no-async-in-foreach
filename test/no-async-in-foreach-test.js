178e69c3c6410da6570ae5ce69ee7fa52c78e8b8
Kelly Selden <kellyselden@gmail.com>
2023-08-28 13:11:53 -0700

    add autofix

--- test/no-async-in-foreach-test.js
+++ test/no-async-in-foreach-test.js
@@ -5,7 +5,7 @@ const rule = require('../src/no-async-in-foreach');
 
 new RuleTester({
   parserOptions: {
-    ecmaVersion: '2017'
+    ecmaVersion: '2021'
   }
 }).run('no-async-in-foreach', rule, {
   valid: [
@@ -18,24 +18,118 @@ new RuleTester({
   ],
   invalid: [
     {
-      code: 'foo.forEach(async function(){})',
+      code: 'async()=>{foo.forEach(async function(){await 1})}',
       errors: [
         {
-          message:
-            'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
           type: 'CallExpression'
         }
-      ]
+      ],
+      output: 'async()=>{for (let i = 0; i < foo.length; i++) {await 1}}'
     },
     {
-      code: 'foo.forEach(async()=>{})',
+      code: 'async()=>{foo.forEach(async function(bar){await bar})}',
       errors: [
         {
-          message:
-            'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
           type: 'CallExpression'
         }
-      ]
+      ],
+      output: 'async()=>{for (let bar of foo) {await bar}}'
+    },
+    {
+      code: 'async()=>{foo.forEach(async(bar)=>{await bar})}',
+      errors: [
+        {
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          type: 'CallExpression'
+        }
+      ],
+      output: 'async()=>{for (let bar of foo) {await bar}}'
+    },
+    {
+      code: 'async()=>{foo.forEach(async(bar,baz)=>{await bar})}',
+      errors: [
+        {
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          type: 'CallExpression'
+        }
+      ],
+      output: 'async()=>{for (let baz = 0; baz < foo.length; baz++) {let bar = foo[baz];await bar}}'
+    },
+
+    // desctructuring and default values
+    {
+      code: 'async()=>{foo.forEach(async(bar=bar2)=>{await bar})}',
+      errors: [
+        {
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          type: 'CallExpression'
+        }
+      ],
+      output: 'async()=>{for (let bar of foo) {bar ??= bar2;await bar}}'
+    },
+    {
+      code: 'async()=>{foo.forEach(async({bar})=>{await bar})}',
+      errors: [
+        {
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          type: 'CallExpression'
+        }
+      ],
+      output: 'async()=>{for (let {bar} of foo) {await bar}}'
+    },
+    {
+      code: 'async()=>{foo.forEach(async({bar}=bar2)=>{await bar})}',
+      errors: [
+        {
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          type: 'CallExpression'
+        }
+      ],
+      output: 'async()=>{for (let i = 0; i < foo.length; i++) {let {bar} = foo[i] ?? bar2;await bar}}'
+    },
+    {
+      code: 'async()=>{foo.forEach(async(bar=bar2,baz=baz2)=>{await bar})}',
+      errors: [
+        {
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          type: 'CallExpression'
+        }
+      ],
+      output: 'async()=>{for (let baz = 0; baz < foo.length; baz++) {let bar = foo[baz] ?? bar2;await bar}}'
+    },
+    {
+      code: 'async()=>{foo.forEach(async({bar},{baz})=>{await bar})}',
+      errors: [
+        {
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          type: 'CallExpression'
+        }
+      ],
+      output: 'async()=>{for (let i = 0; i < foo.length; i++) {let {bar} = foo[i];await bar}}'
+    },
+    {
+      code: 'async()=>{foo.forEach(async({bar}=bar2,{baz}=baz2)=>{await bar})}',
+      errors: [
+        {
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          type: 'CallExpression'
+        }
+      ],
+      output: 'async()=>{for (let i = 0; i < foo.length; i++) {let {bar} = foo[i] ?? bar2;await bar}}'
+    },
+
+    // no block in arrow func
+    {
+      code: 'async()=>{foo.forEach(async(bar)=>await bar)}',
+      errors: [
+        {
+          message: 'An async callback inside `forEach` swallows promises. You should either convert to `for...of` syntax, or swap `forEach` for `map` and wrap in a `Promise.all`.',
+          type: 'CallExpression'
+        }
+      ],
+      output: 'async()=>{for (let bar of foo) {await bar}}'
     }
   ]
 });
